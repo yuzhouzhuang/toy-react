7d4e92e97bac753f02283046abc2fb7761ef686b
Yuzhou Zhuang <yuzhou.zhuang19@gmail.com>
2020-09-18 11:26:27 +0100

    feat(*): finish second version

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-import {createElement, Component, render} from './toy-react.js'
+import { createElement, Component, render } from './toy-react.js'
 
 class MyComponent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            a: 1,
+            b: 2,
+        }
+    }
+
     render() {
         return <div>
             <h1>my component</h1>
-            {this.children}
+            <button onclick={() => { this.setState({ a: this.state.a + 1 }) }}>add</button>
+            <span>{this.state.a.toString()}</span>
+
         </div>;
     }
 }
